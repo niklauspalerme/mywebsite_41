0971e5f9ec6d077747de6901ae59c8f932a378f4
Palermo Rodriguez <nicolas.palermo@kcc.com>
2021-10-14 19:11:52 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,12 @@ module.exports = {
     env_local: {
       "NODE_ENV": "local",
       "API_DESCRIPTION": "Estás ejecutando tu API en modo desarrollador.",
-      "DB_PASS": "1234567890",
+      "DB_PASS": "1234567890"
     },
     env_production: {
       "NODE_ENV": "production",
       "API_DESCRIPTION": "Estás ejecutando tu API en producción. ¡¡Ten cuidado!!",
-      "DB_PASS": "1234567890",
+      "DB_PASS": "1234567890"
     }
   }]
 };
